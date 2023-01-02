c10664821e27771c2507086874835cf2194ad762
zero86 <katanazero86@gmail.com>
2023-01-02 19:42:53 +0900

    - test 용 앱키 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <TmapGeoPostcode
-        appKey={"l7xx5680091346d34dd69f230143577d1e37"}
+        appKey={""}
         count={10}
         onSuccess={(targetRow) => console.log(targetRow)}
       />
